087f647c4b3e946a24110ebbaffc1a164ed2362b
ROSA GENETTI <rgenetti@lesley.edu>
2019-09-22 22:28:49 -0400

    changed CSS

--- blog_cms-master/index.php
+++ blog_cms-master/index.php
@@ -4,26 +4,32 @@
                 Hello World CMS Blog
                 </title>
                 <link rel="stylesheet" type="text/css" href="format.css" />
+                
+                        <div id="header">
     <header><h1>CMS Blog</h1></header>
     <nav>
             <a href="/index.html" >Home</a> |
             <a href="/post-submission.html">Submit A Post</a> |
+
             
           </nav> 
-          
+          </div> <!--close for header-->
           
           <body>
-
+                <div id="content">
                 <main>
                         <ul>
                                 <li><a href="/post.html"> Post 1</a></li>
                                 
                               </ul> 
                 </main>
+        </div> <!--close for content-->
+        <div id="footer">
                 <footer>
                         <p>Created by Rosa Genetti 2019</p>
                 
                       </footer>     
 
+                      </div>
 </head>
 </html>
